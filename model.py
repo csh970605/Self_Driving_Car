68c4c6d14de99c753db66e46bb2fb97e9938462b
SeHoonC <csh970605@gmail.com>
2023-08-09 12:04:45 +0900

    Update model.py

--- model.py
+++ model.py
@@ -60,18 +60,17 @@ class Dqn():
         self.last_reward = 0
     
     def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        probs = F.softmax(self.model(Variable(state, volatile=True))*100) # T=100
         action = probs.multinomial(num_samples=1)
         return action.data[0,0]
     
-    def train(self, batch_state, batch_next_state, batch_reward, batch_action):
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        print(outputs)
         next_outputs = self.model(batch_next_state).detach().max(1)[0]
         target = self.gamma*next_outputs + batch_reward
         td_loss = F.smooth_l1_loss(outputs, target)
         self.optimizer.zero_grad()
-        td_loss.backward(retain_graph = True)
+        td_loss.backward(retain_graph=True)
         self.optimizer.step()
     
     def update(self, reward, new_signal):
@@ -79,8 +78,8 @@ class Dqn():
         self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
         action = self.select_action(new_state)
         if len(self.memory.memory) > 100:
-            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
-            self.train(batch_state, batch_next_state, batch_reward, batch_action)
+            batch_state, batch_next_state, batch_action, batch_reward=self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
         self.last_action = action
         self.last_state = new_state
         self.last_reward = reward
